1da937d775b92a1b8b37aea835d59d4394fe99be
JiHoon Kang <deferare@icloud.com>
2024-01-17 06:40:01 +0900

    마이 페이지 뷰 (이용 중일 때) 틀 잡음

---
+++ NBCSuper8oard/Controller/MyPageViewController.swift
@@ -0,0 +1,36 @@
+//
+//  MyPageViewController.swift
+//  NBCSuper8oard
+//
+//  Created by JiHoon K on 1/16/24.
+//
+
+import UIKit
+
+class MyPageViewController: UIViewController {
+    @IBOutlet weak var name: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    
+    override func viewDidAppear(_ animated: Bool) {
+
+    }
+}
+
+extension MyPageViewController {
+    @IBAction private func tappedReturn() {
+        
+    }
+}
+
+
+
+//@available(iOS 17, *)
+//#Preview(traits: .defaultLayout) {
+//    return MyPageViewController()
+//}
