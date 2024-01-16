2dbc1a6b75bf1d318e437d312a5d97a44308087c
y0unghyun <dl3rpika@gmail.com>
2024-01-16 11:27:25 +0900

    initial commit

---
+++ NBCSuper8oard/Model/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  NBCSuper8oard
+//
+//  Created by 영현 on 1/16/24.
+//
+
+import Foundation
+
+struct User {
+    var id: String
+    var password: String
+    var name: String
+    var phoneNumber: String
+    var email: String
+    var birthDate: String
+}
